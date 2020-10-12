48a97bf84c4ee20b61739a3eb0ec902fe7c1167e
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-10-11 21:50:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>
+        <div>
+          <span>
+            <h1><a href="Home" className="App-link">MEDLEY</a></h1>
+          </span>
+          <h9>Flavors we bring to the table</h9> 
+        </div>
+        </>
       </header>
     </div>
   );
